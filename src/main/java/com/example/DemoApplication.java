73749599aace73078a63d3acc1784c49230d7049
Gunnar Hillert <ghillert@pivotal.io>
2016-05-26 11:14:46 -0400

    Initial commit

---
+++ src/main/java/com/example/DemoApplication.java
@@ -0,0 +1,52 @@
+	package com.example;
+
+	import java.security.Principal;
+import java.util.Collections;
+import java.util.Map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.authserver.OAuth2AuthorizationServerConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	@RequestMapping({"/user", "/me"})
+    public Map<String, String> user(Principal principal) {
+        return Collections.singletonMap("name", principal.getName());
+    }
+
+    @Configuration
+    @EnableAuthorizationServer
+    protected static class MyOAuth2AuthorizationServerConfiguration extends OAuth2AuthorizationServerConfiguration {
+        @Override
+        public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+            super.configure(security);
+            security.allowFormAuthenticationForClients();
+        }
+    }
+
+    @Configuration
+    @EnableResourceServer
+    protected static class ResourceServerConfiguration
+            extends ResourceServerConfigurerAdapter {
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            http.antMatcher("/me").authorizeRequests().anyRequest().authenticated();
+        }
+    }
+
+}
